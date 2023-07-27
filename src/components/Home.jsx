7a86bb88e7e3c8e63e72d7fc612783828d1e32a4
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-27 17:00:49 +0200

    fix grid container structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,14 @@
 const Home = () => {
     return (
         <div className="home container-fluid">
-            <div className="row h-100">
-                <div className="text-container col-3 col-md-12 h-50">
+            <div className="row h-50">
+                <div className="text-container col-12  h-100">
                     <p className="name-font">SALVATORE QUAGLIARIELLO</p>
                     <p className="p-font">//Front-end Developer</p>
                 </div>
-                <section className="links-container col-9 col-md-12 h-50">
+            </div>
+            <div className="row h-50">
+                <section className="links-container col-12  h-100">
                             <h2 className="link-text col-12">WORKS</h2>
                             <h2 className="link-text col-12">ABOUT</h2>
                             <h2 className="link-text col-12">CONTACT</h2>

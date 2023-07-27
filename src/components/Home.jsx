9f03438b3fd296420a1a810222bb11c2fdf7bce1
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-27 10:37:35 +0200

    add light-theme and dark-theme css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,12 @@
 
 const Home = () => {
     return (
-        <div className="home container-fluid">
-            <h1 className="header-font">Salvatore Quagliariello</h1>
+        <div className="home container">
+            <div>
+                <p className="p-font">Hi, i am <span className="header-font">Salvatore Quagliariello</span>, pleased to "meet" you.</p>
+                <h2 className="p-font">a Front-end Developer</h2>
+            </div>
+            <div></div>
         </div>
     )
 }

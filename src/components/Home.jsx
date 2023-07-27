d59ea1c9c05acb90e87102dad39fb761e5d93938
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-27 16:42:41 +0200

    modify home structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,18 @@
 
 const Home = () => {
     return (
-        <div className="home container">
-            <div>
-                <p className="p-font">Hi, i am <span className="header-font">Salvatore Quagliariello</span>, pleased to "meet" you.</p>
-                <h2 className="p-font">a Front-end Developer</h2>
+        <div className="home container-fluid">
+            <div className="row h-100">
+                <div className="text-container col-3 col-md-12 h-50">
+                    <p className="name-font">SALVATORE QUAGLIARIELLO</p>
+                    <p className="p-font">//Front-end Developer</p>
+                </div>
+                <section className="links-container col-9 col-md-12 h-50">
+                            <h2 className="link-text col-12">WORKS</h2>
+                            <h2 className="link-text col-12">ABOUT</h2>
+                            <h2 className="link-text col-12">CONTACT</h2>
+                </section>
             </div>
-            <div></div>
         </div>
     )
 }

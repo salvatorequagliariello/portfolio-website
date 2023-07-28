17cf06776eded17b74b813d964f939be19aeb779
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-28 17:54:52 +0200

    add projectview structure

--- src/components/ProjectView.jsx
+++ src/components/ProjectView.jsx
@@ -1,4 +1,14 @@
 
-const ProjectView = () => {
-    
-}
+const ProjectView = ({projectImage, projectName, projectTech, projectLink, projectDescription}) => {
+    return (
+        <div>
+            <img className="project-view__image" src={projectImage}></img>
+            <p className="project-view__title">{projectName}</p>
+            <p className="project-view__tech">{projectTech}</p>
+            <p className="project-view__link">{projectLink}</p>
+            <p className="project-view__description">{projectDescription}</p>
+        </div>
+    );
+};
+
+export default ProjectView;
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
 
 import GoBack from "./GoBackNavigation";
+import ProjectView from "./ProjectView";
+
+import facebookImage from '../assets/regular-facebook-design-new.png';
+
+const project = {
+    title: "facebook",
+    image: facebookImage,
+    tech: "React, CSS",
+    link: "facebook.com",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac massa rhoncus, tempor augue a, euismod mi. Maecenas mollis arcu vitae nunc fermentum, sed lobortis nisl rhoncus. Morbi tincidunt neque et augue luctus semper. Quisque sed luctus risus. Donec ut risus elit."
+};
 
 const Works = () => {
     return (
@@ -16,7 +27,14 @@ const Works = () => {
                         <p className="works__link">Giovannino</p>
                         <p className="works__link">Netflix</p>
                     </div>
-                    <div className="col-6 h-100"></div>
+                    <div className="col-6 h-100">
+                        <ProjectView 
+                        projectImage={project.image}
+                        projectName={project.title}
+                        projectTech={project.tech}
+                        projectLink={project.link}
+                        projectDescription={project.description} />
+                    </div>
                 </div>
             </div>
     )

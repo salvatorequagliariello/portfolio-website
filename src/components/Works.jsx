d6af1a7c89d4810779c2500dfa90354b8cb96a00
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-28 12:08:24 +0200

    complete links column, works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,9 +4,21 @@ import GoBack from "./GoBackNavigation";
 
 const Works = () => {
     return (
-        <>
-            <GoBack categoryName={"WORKS"}/>
-        </>
+            <div className="works-page container-fluid  h-100">
+                <GoBack categoryName={"WORKS"}/>
+                <div className="row h-100">
+                    <div className="works__links-list col-6 h-100">
+                        <p className="works__link">Instagram</p>
+                        <p className="works__link">Facebook</p>
+                        <p className="works__link">Youtube</p>
+                        <p className="works__link">Twitter</p>
+                        <p className="works__link">Snapchat</p>
+                        <p className="works__link">Giovannino</p>
+                        <p className="works__link">Netflix</p>
+                    </div>
+                    <div className="col-6 h-100"></div>
+                </div>
+            </div>
     )
 }
 
